bec24ff214b003bca5a94b8683f908727e65095f
Jacky Tsang <szechunt@gmail.com>
2019-08-07 02:10:34 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a6b86ca1b518781b19fa.js"
+    "url": "webpack-runtime-248a726dd6715ab47cad.js"
   },
   {
     "url": "app-dea32ae5aaff4f47d445.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e04e937ccdc9f0baabb96fcb10482d06"
+    "revision": "8054cd49ea77e722ef0af0bfff0170ab"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
