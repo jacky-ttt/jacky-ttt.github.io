ee116cccd3ba34c6c3366849acb8378b435835fd
Jacky Tsang <szechunt@gmail.com>
2022-12-14 23:39:06 +0800

    update link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -150,16 +150,21 @@ const IndexPage = () => {
         </Subtitle>
 
         <div className="grid grid-rows-2 xl:grid-rows-1 xl:grid-cols-2 gap-4 mt-20">
+        <LinkCard
+            title="LinkedIn"
+            subtitle="There records my career path, my skills, my language, my whole."
+            link="https://www.linkedin.com/in/jacky-tsang-93646a193/"
+            gradientBg="bg-gradient-to-r from-blue-800 to-cyan-500" />
           <LinkCard
             title="GitHub"
             subtitle="A developer needs a Github to support him somewhere in his life."
             link="https://github.com/jacky-ttt/"
-            gradientBg="bg-gradient-to-r from-blue-800 to-cyan-500" />
+            gradientBg="bg-gradient-to-r from-violet-800 to-fuchsia-500" />
           <LinkCard
             title="Medium"
             subtitle="I post articles on Medium now and then. Mostly about the dev I used in my work."
             link="https://jacky-ttt.medium.com/"
-            gradientBg="bg-gradient-to-r from-violet-800 to-fuchsia-500" />
+            gradientBg="bg-gradient-to-r from-green-800 to-emerald-500" />
         </div>
 
         <p className="mt-20 mb-4 text-2xl font-sans text-white">Projects</p>
