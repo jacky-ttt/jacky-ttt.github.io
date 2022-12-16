6715389741e747bb077b221bc3af0e691b04966c
Jacky Tsang <szechunt@gmail.com>
2022-12-17 01:11:31 +0800

    white text

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Link, HeadFC } from "gatsby"
 
 const pageStyles = {
-  color: "#232129",
+  color: "#ffffff",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
